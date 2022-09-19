bd912fa6193104efee62e2970aa017a51c2ae097
nishina555 <n.toshi.1.014@gmail.com>
2022-09-19 23:56:27 +0900

    fix css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,10 @@
 import TodoApp from "../TodoApp";
+import styles from "./App.module.css";
 
 export default function App() {
-  return <TodoApp />;
+  return (
+    <div className={styles.container}>
+      <TodoApp />
+    </div>
+  );
 }
