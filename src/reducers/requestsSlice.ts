f0df33471a399f33168bc281de444d82971fdbb4
nishina555 <n.toshi.1.014@gmail.com>
2022-10-01 20:47:22 +0900

    getTods.fullfilled

--- src/reducers/requestsSlice.ts
+++ src/reducers/requestsSlice.ts
@@ -1,11 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getTodos } from "./todosSlice";
 
-const initialState = {};
+const initialState = {
+  getTodos: {
+    status: "request",
+  },
+};
 
 export const requestSlice = createSlice({
   name: "request",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getTodos.fulfilled, (state, action) => {
+      state["getTodos"] = {
+        status: "fullfilled",
+      };
+    });
+  },
 });
 
 export default requestSlice.reducer;
