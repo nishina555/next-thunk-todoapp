d640df2a5761a83c87ee4267b1be60740ac44228
nishina555 <n.toshi.1.014@gmail.com>
2022-10-02 16:38:21 +0900

    add selector

--- src/selectors/requests.ts
+++ src/selectors/requests.ts
@@ -8,16 +8,41 @@ const selectRequests = (state: AppState): RequestState => state.requests;
 const selectRequest = (actionType: string) =>
   createSelector([selectRequests], (requests) => requests[actionType] || {});
 
+// リクエストの状態を取得
 const selectRequestStatus = (actionType: string) =>
   createSelector([selectRequest(actionType)], (request) => request?.status);
 
+// リクエスト中の場合trueを返す
 export const selectHasRequestStarted = (actionType: string) =>
   createSelector(
     [selectRequest(actionType)],
-    (request) => request?.status !== undefined
+    (request) => request?.status === RequestStatus.Request
   );
 
-export const selectHasRequestCompleted = (actionType: string) =>
+// リクエスト成功の場合trueを返す
+export const selectHasRequestSucceeded = (actionType: string) =>
+  createSelector(
+    [selectRequest(actionType)],
+    (request) => request?.status === RequestStatus.Success
+  );
+
+// リクエスト失敗の場合trueを返す
+export const selectHasRequestFailed = (actionType: string) =>
+  createSelector(
+    [selectRequest(actionType)],
+    (request) => request?.status === RequestStatus.Failure
+  );
+
+// リクエスト未完了（未実行、実行中）の場合trueを返す
+export const selectHasRequestNotDone = (actionType: string) =>
+  createSelector(
+    [selectRequest(actionType)],
+    (request) =>
+      request?.status === RequestStatus.Request || request?.status === undefined
+  );
+
+// リクエスト完了の場合trueを返す（リクエストの成否は問わない）
+export const selectHasRequestDone = (actionType: string) =>
   createSelector([selectRequest(actionType)], (request) => {
     const complatedRequestStatuses = [
       RequestStatus.Failure,
