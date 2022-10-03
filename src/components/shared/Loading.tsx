d0be2a8b7f2f2346211d094780e8ac9dd35f4944
nishina555 <n.toshi.1.014@gmail.com>
2022-10-03 20:13:59 +0900

    update

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -1,26 +1,14 @@
 import { FC } from "react";
-// import loadingCircleIcon from "../../../public/images/loadingCircleGray.png";
+import loadingCircleIcon from "../../../public/images/loadingCircleGray.png";
 import styles from "./Loading.module.css";
-
-// export const Loading: FC = () => {
-//   return (
-//     <div className={styles.container}>
-//       <p>ロード中</p>
-//     </div>
-//   );
-// };
-
-// export const Loading: FC = () => {
-//   return (
-//     <div className={styles.container}>
-//       {/* <img className={styles.icon} src={loadingCircleIcon.src} /> */}
-//     </div>
-//   );
-// };
 
 export const Loading: FC = () => {
   return (
     <div className={styles.container}>
+      {/* pngファイルを利用する場合 */}
+      {/* <img className={styles.icon} src={loadingCircleIcon.src} /> */}
+
+      {/* svgファイルを利用する場合 */}
       <img className={styles.icon} src="images/loading.svg" />
     </div>
   );
